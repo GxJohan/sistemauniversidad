3d02b0534a8322a2bedc65e926fe148fdfe338ce
C27444 <c27444@utp.edu.pe>
2024-09-28 20:18:02 -0500

    v1.7

--- src/main/java/com/example/sistemauniversidad/SistemaUniversidadApplication.java
+++ src/main/java/com/example/sistemauniversidad/SistemaUniversidadApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SistemaUniversidadApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SistemaUniversidadApplication.class, args);
     }
 
---
+++ src/main/java/com/example/sistemauniversidad/controller/StudentController2.java
@@ -0,0 +1,20 @@
+package com.example.sistemauniversidad.controller;
+
+import com.example.sistemauniversidad.model.Student;
+import com.example.sistemauniversidad.service.StudentService;
+import com.example.sistemauniversidad.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/students")
+public class StudentController2 {
+
+
+
+
+}
