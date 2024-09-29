8cecbf5bcff9d794421b1f84f0d177cf492a486f
C27444 <c27444@utp.edu.pe>
2024-09-28 20:39:38 -0500

    v1.8

--- src/main/java/com/example/sistemauniversidad/controller/StudentController2.java
+++ src/main/java/com/example/sistemauniversidad/controller/StudentController2.java
@@ -7,12 +7,43 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
 
 import java.util.List;
 
 @Controller
 @RequestMapping("/students")
 public class StudentController2 {
+
+    @Autowired
+    private StudentService studentService;
+
+    //Página principal
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+
+    //Listar Alumnos
+    @GetMapping("/listar")
+    public String listar(Model model) {
+        model.addAttribute("students", studentService.getAllStudents());
+        return "listar";
+    }
+
+    //Mostrar formulario para Agregar Alumno
+    @GetMapping("/crear")
+    public String crear(Model model) {
+        model.addAttribute("student", new Student());
+        return "crear";
+    }
+
+    //Accion de Agregar Alumno
+    @PostMapping("/guardar")
+    public String guardar(@ModelAttribute Student student) {
+        studentService.addStudent(student);
+        return "redirect:/students/listar";
+    }
 
 
 
