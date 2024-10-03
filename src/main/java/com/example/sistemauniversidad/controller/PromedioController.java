2addb77c907b1a43ee654210c7b0238ef6b53b05
C27444 <c27444@utp.edu.pe>
2024-10-02 19:59:13 -0500

    v1.9.1 - Creación de Modelo Promedio

---
+++ src/main/java/com/example/sistemauniversidad/controller/PromedioController.java
@@ -0,0 +1,11 @@
+package com.example.sistemauniversidad.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.example.sistemauniversidad.service.PromedioService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+
+public class PromedioController {
+}
