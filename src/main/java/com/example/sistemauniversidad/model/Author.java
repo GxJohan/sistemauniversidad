4310ca9d67912613ed8b7c14a7612a4880676efa
C27444 <c27444@utp.edu.pe>
2024-10-26 20:10:43 -0500

    v1.9.1 - Creación de Modelo Promedios

---
+++ src/main/java/com/example/sistemauniversidad/model/Author.java
@@ -0,0 +1,32 @@
+package com.example.sistemauniversidad.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+import java.util.List;
+
+
+@Entity
+@Table(name = "author")
+public class Author {
+
+    @Id//Define este campo como la clave primaria de la tabla
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//Genera un valor automático para este campo autoincremental
+    private Long id;
+
+    @NotNull(message = "El nombre del autor es requerido")//Valida que el campo no sea nulo
+    @Size(min = 3, max = 50, message = "    El nombre del autor debe tener entre 3 y 50 caracteres")//Valida que el campo tenga una longitud entre 3 y 50 caracteres
+    private String name;
+
+    @NotNull(message = "El correo del autor es requerido")//Valida que el campo no sea nulo
+    @Column(unique = true)//Valida que el campo sea único
+    @Pattern(regexp = "^[a-zA-Z0-9_.%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "El correo del autor no es válido")//Valida que el campo cumpla con una expresión regular
+    private String email;
+
+
+
+
+}
