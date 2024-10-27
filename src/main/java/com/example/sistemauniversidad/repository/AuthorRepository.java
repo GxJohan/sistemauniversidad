c4fb0f287f8b87b043e92883dfdfcd752d64226a
C27444 <c27444@utp.edu.pe>
2024-10-26 20:34:54 -0500

    v2.1

---
+++ src/main/java/com/example/sistemauniversidad/repository/AuthorRepository.java
@@ -0,0 +1,10 @@
+package com.example.sistemauniversidad.repository;
+
+import com.example.sistemauniversidad.model.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+    Author findByName(String name);
+}
---
+++ src/main/java/com/example/sistemauniversidad/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package com.example.sistemauniversidad.repository;
+
+import com.example.sistemauniversidad.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+    Book findByTitle(String title);
+}
